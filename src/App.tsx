80c8d37d33872c2a91b63e67ba4a572b31456ea6
deanhan68 <savelynikolaev@mail.ru>
2024-12-04 18:35:01 +0500

    создал проект

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, World!96</h1> {/* Просто текст для проверки */}
+    </div>
+  );
+};
+
+export default App;
